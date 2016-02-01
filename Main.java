8e4bc5b97cdb0267b6e3daa254a926c7780d1830
Ishkov Victor <razuma_net@bk.ru>
2016-02-01 22:35:59 +0300

    first commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+	private String git;
+
+	public Main() {
+		this.git = "Git Test";
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
